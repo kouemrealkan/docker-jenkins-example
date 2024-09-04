77b50b4b0b5d338dbc0317839b195f407328e951
volantx.ealkan <emre.alkan@volantx.com>
2024-09-04 20:20:29 +0300

    task update init

--- src/main/java/com/alkan/docker_example/TaskController.java
+++ src/main/java/com/alkan/docker_example/TaskController.java
@@ -17,6 +17,11 @@ public class TaskController {
         return taskRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Task getTask(@PathVariable String id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
     @PostMapping
     public void save(@RequestBody Task task) throws Exception {
         var isExist = taskRepository.findByName(task.getName());
@@ -25,4 +30,15 @@ public class TaskController {
         }
         taskRepository.save(task);
     }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable String id, @RequestBody Task updatedTask) throws Exception {
+        var task = taskRepository.findById(id).orElse(null);
+        if (task != null) {
+            task.setName(updatedTask.getName());
+            task.setDescription(updatedTask.getDescription());
+            taskRepository.save(task);
+        }
+    }
+
 }
